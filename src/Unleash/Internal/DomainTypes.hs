5ff685f23e147ecd60a02d31227df6a27a33b0bc
Even Brenden <evenbrenden@gmail.com>
2023-01-19 10:14:12 +0100

    Refactor predicates

--- src/Unleash/Internal/DomainTypes.hs
+++ src/Unleash/Internal/DomainTypes.hs
@@ -18,15 +18,12 @@ import Data.Maybe (catMaybes, fromMaybe, listToMaybe, maybeToList)
 import Data.Text (Text)
 import qualified Data.Text as Text
 import Data.Text.Encoding (encodeUtf8)
-import Data.Time (UTCTime, ZonedTime, zonedTimeToUTC)
-import Data.Time.Format.ISO8601 (iso8601ParseM)
-import Data.Versions (SemVer, semver)
 import Data.Word (Word32)
 import System.Random (randomRIO)
-import Text.Read (readMaybe)
 import TextShow (showt)
 import Unleash.Internal.JsonTypes (Variant, VariantResponse (..), emptyVariantResponse)
 import qualified Unleash.Internal.JsonTypes as JsonTypes
+import Unleash.Internal.Predicates (datePredicate, numPredicate, semVerPredicate)
 
 supportedStrategies :: [Text]
 supportedStrategies = ["default", "userWithId", "gradualRolloutUserId", "gradualRolloutSessionId", "gradualRolloutRandom", "remoteAddress", "flexibleRollout"]
@@ -311,44 +308,6 @@ containsAnyOf mCurrentValue values = do
     case mCurrentValue of
         Nothing -> False
         Just currentValue -> any (`Text.isInfixOf` currentValue) values
-
--- TODO: extract predicates to its own module
-numPredicate :: (Double -> Double -> Bool) -> Maybe Text -> Maybe Text -> Bool
-numPredicate predicate mCurrentValue mConstraintValue = do
-    let maybeCurrentValue :: Maybe Double = readMaybe . Text.unpack =<< mCurrentValue
-    let maybeConstraintValue :: Maybe Double = readMaybe . Text.unpack =<< mConstraintValue
-
-    case (maybeConstraintValue, maybeCurrentValue) of
-        (Just constraintValue, Just currentValue) -> predicate currentValue constraintValue
-        _ -> False
-
-datePredicate :: (UTCTime -> UTCTime -> Bool) -> Maybe Text -> Maybe Text -> Bool
-datePredicate predicate mCurrentValue mConstraintValue = do
-    let parseDate :: Text -> Maybe UTCTime
-        parseDate text =
-            (iso8601ParseM @Maybe @UTCTime $ Text.unpack text)
-                <|> zonedTimeToUTC <$> (iso8601ParseM @Maybe @ZonedTime $ Text.unpack text)
-
-    let mConstraintDate :: Maybe UTCTime = parseDate =<< mConstraintValue
-    let mProvidedDate :: Maybe UTCTime = parseDate =<< mCurrentValue
-
-    case (mProvidedDate, mConstraintDate) of
-        (Just providedDate, Just constraintDate) -> predicate providedDate constraintDate
-        _ -> False
-
-semVerPredicate :: (SemVer -> SemVer -> Bool) -> Maybe Text -> Maybe Text -> Bool
-semVerPredicate predicate mCurrentValue mConstraintValue = do
-    let eitherToMaybe e =
-            case e of
-                Right a -> Just a
-                _ -> Nothing
-
-    let mConstraintSemVer :: Maybe SemVer = eitherToMaybe . semver =<< mConstraintValue
-    let mProvidedSemVer :: Maybe SemVer = eitherToMaybe . semver =<< mCurrentValue
-
-    case (mProvidedSemVer, mConstraintSemVer) of
-        (Just providedSemVer, Just constraintSemVer) -> predicate providedSemVer constraintSemVer
-        _ -> False
 
 getNormalizedNumberN :: Text -> Text -> Word32 -> Int
 getNormalizedNumberN identifier groupId n = do
